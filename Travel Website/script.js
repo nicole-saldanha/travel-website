a0b4c3b575cd6e661a1d0a4a58c5f08f9b964842
nicole-saldanha <112466845+nicole-saldanha@users.noreply.github.com>
2024-04-05 10:30:55 +0530

    Add files via upload

---
+++ Travel Website/script.js
@@ -0,0 +1,14 @@
+
+    
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    setTimeout(function() {
+        loader.style.display = "none";
+        document.body.classList.add('scroll-enable');
+        }, 0);
+  
+  
+})
+
+
+     
